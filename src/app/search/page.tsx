a5faef65b9698b4393094b766d2f9514d83c0bad
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-17 14:31:39 +0100

    Added pages

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+export default function Search() {
+	return (
+		<main>
+			<form action="">
+				<label htmlFor="searchForPokemon" />
+				<input type="text" id="searchForPokemon" />
+				<button type="submit">Search</button>
+			</form>
+		</main>
+	);
+}
