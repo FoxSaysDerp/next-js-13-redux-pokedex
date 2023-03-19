c8c7e84a00871a38cf371d664e94348aeb53d8b6
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-19 12:54:10 +0100

    Added styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
+import s from "./page.module.scss";
+
 export default async function Home() {
 	return (
-		<main>
-			Pokedex
-			<Link href="search">Go to Search</Link>
+		<main className={s.mainContainer}>
+			<h1 className={s.title}>Pokédex</h1>
+			<Link href="search" className="button">
+				Go to Search
+			</Link>
 		</main>
 	);
 }
