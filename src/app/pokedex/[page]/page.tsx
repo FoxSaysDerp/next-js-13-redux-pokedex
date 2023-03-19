6a4fe0577009100a2f36b87f41019978cb394b55
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-19 14:32:01 +0100

    Added Pagination

--- src/app/pokedex/[page]/page.tsx
+++ src/app/pokedex/[page]/page.tsx
@@ -1,4 +1,4 @@
-import { Pagination } from "@mantine/core";
+import Link from "next/link";
 import { getAllPokemonByPage } from "@/api/getAllPokemonByPage";
 import PokemonTable from "@/components/PokemonTable/PokemonTable";
 interface PageProps {
@@ -11,9 +11,19 @@ export default async function PokedexSinglePage(props: PageProps) {
 	const { params } = props;
 	const data = await getAllPokemonByPage(params.page);
 
+	const totalPages = Math.ceil(data.count / 20);
+
+	const isFirst = Number(params.page) === 1;
+	const isLast = Number(params.page) === totalPages;
+
+	const prevPageIndex = "pokedex/" + (Number(params.page) - 1).toString();
+	const nextPageIndex = "pokedex/" + (Number(params.page) + 1).toString();
+
 	return (
 		<div>
 			<PokemonTable pokemon={data.results} />
+			{!isFirst && <Link href={prevPageIndex}>Previous page</Link>}
+			{!isLast && <Link href={nextPageIndex}>Next page</Link>}
 		</div>
 	);
 }
